b0a6594ae8b7c341b8b8cbf009a3e96f70215374
Shane <croconoster@gmail.com>
2012-08-06 11:07:53 +1200

    Initial Code for project

---
+++ Battle for Adrastea/src/MainGame/Main.java
@@ -0,0 +1,22 @@
+package MainGame;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.terrain.geomipmap.TerrainQuad;
+
+/**
+ * test
+ * @author normenhansen
+ */
+public class Main extends SimpleApplication {
+    
+    public static void main(String[] args) {
+        Main app = new Main();
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        
+    }
+}
