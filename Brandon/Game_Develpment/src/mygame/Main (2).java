96a53cd6862c4d5ee0c66bb7624b16c31415c9bd
Brandon Dawson <kewlbrandine@hotmail.com>
2012-08-06 12:25:00 +1200

    2nd tank added

---
+++ Brandon/Game_Develpment/src/mygame/Main (2).java
@@ -0,0 +1,45 @@
+package mygame;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.light.DirectionalLight;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.RenderManager;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Spatial;
+import com.jme3.scene.shape.Box;
+
+/**
+ * test
+ * @author normenhansen
+ */
+public class Main extends SimpleApplication {
+
+    public static void main(String[] args) {
+        Main app = new Main();
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        
+Spatial scene = assetManager.loadModel("Scenes/Tankscene.j3o");
+rootNode.attachChild(scene);
+        
+       DirectionalLight sun = new DirectionalLight();
+sun.setColor(ColorRGBA.White);
+sun.setDirection(new Vector3f(-.5f,-.5f,-.5f).normalizeLocal());
+rootNode.addLight(sun); 
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        //TODO: add update code
+    }
+
+    @Override
+    public void simpleRender(RenderManager rm) {
+        //TODO: add render code
+    }
+}
