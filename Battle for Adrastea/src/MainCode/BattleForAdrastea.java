7cedea1c7e3f00d6d2cca22036fbbabf41070e10
Shane <croconoster@gmail.com>
2012-08-07 15:33:35 +1200

    Added Settings in

--- Battle for Adrastea/src/MainCode/BattleForAdrastea.java
+++ Battle for Adrastea/src/MainCode/BattleForAdrastea.java
@@ -5,33 +5,25 @@ import com.jme3.asset.plugins.FileLocator;
 import com.jme3.bounding.BoundingBox;
 import com.jme3.bullet.BulletAppState;
 import com.jme3.bullet.PhysicsSpace;
-import com.jme3.bullet.collision.shapes.BoxCollisionShape;
 import com.jme3.bullet.collision.shapes.CollisionShape;
-import com.jme3.bullet.collision.shapes.CompoundCollisionShape;
-import com.jme3.bullet.collision.shapes.HullCollisionShape;
 import com.jme3.bullet.control.RigidBodyControl;
 import com.jme3.bullet.control.VehicleControl;
 import com.jme3.bullet.util.CollisionShapeFactory;
-import com.jme3.input.ChaseCamera;
 import com.jme3.input.KeyInput;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.KeyTrigger;
 import com.jme3.light.AmbientLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
-import com.jme3.math.FastMath;
-import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
-import com.jme3.scene.shape.Cylinder;
 import com.jme3.system.AppSettings;
 import com.jme3.terrain.geomipmap.TerrainLodControl;
 import com.jme3.terrain.geomipmap.TerrainQuad;
 import com.jme3.terrain.heightmap.AbstractHeightMap;
 import com.jme3.terrain.heightmap.ImageBasedHeightMap;
 import com.jme3.texture.Texture;
-import java.util.List;
 
 
 public class BattleForAdrastea extends SimpleApplication implements ActionListener{
@@ -40,7 +32,7 @@ public class BattleForAdrastea extends SimpleApplication implements ActionListen
     private Material mat_terrain;
     private BulletAppState bulletAppState;
     private VehicleControl vehicleControl;
-    private RigidBodyControl landscape; //Terrian solid
+    private RigidBodyControl landscape; 
     
     //Vehicle stuff
     private float wheelRadius;
@@ -52,13 +44,18 @@ public class BattleForAdrastea extends SimpleApplication implements ActionListen
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        AppSettings settings = new AppSettings(true);
-        settings.setResolution(1600, 900);
-        settings.setVSync(true);
-        settings.setFullscreen(false);
+        AppSettings gameSettings = new AppSettings(true);
+        gameSettings.setResolution(1920, 1080);
+        //gameSettings.setFullscreen(true);
+        gameSettings.setVSync(true);
+        gameSettings.setTitle("Game");
+        gameSettings.setUseInput(true);
+        gameSettings.setFrameRate(500);
+        gameSettings.setSamples(0);
+        gameSettings.setRenderer("LWJGL-OpenGL2");
         
         BattleForAdrastea app = new BattleForAdrastea();        
-        app.setSettings(settings);
+        app.setSettings(gameSettings);
         app.setShowSettings(false);
         app.start();
     }
