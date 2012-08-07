97ffd924a7403467e74ad56b8b983533c1ee8190
Shane <croconoster@gmail.com>
2012-08-07 19:21:12 +1200

    Introduced methods

--- Battle for Adrastea_new/src/mygame/Game.java
+++ Battle for Adrastea_new/src/mygame/Game.java
@@ -20,20 +20,29 @@ public class Game extends SimpleApplication{
     public void simpleInitApp() {       
         flyCam.setMoveSpeed(50);
         assetManager.registerLocator("./assets", FileLocator.class);
+        setUpLighting();
+        setUpWorldTerrain();    
+        setUpTank();
         
+    }
+
+    private void setUpLighting() {
         DirectionalLight directionalLight = new DirectionalLight();
         directionalLight.setDirection(new Vector3f(-0.1f, -1f, -1).normalizeLocal());
         rootNode.addLight(directionalLight);
-        
+    }
+
+    private void setUpWorldTerrain() {
         Spatial sceneModel = assetManager.loadModel("Scenes/WorldScene.j3o");
         sceneModel.setLocalTranslation(0,0,0);
-        rootNode.attachChild(sceneModel);    
-        
+        rootNode.attachChild(sceneModel);
+    }
+
+    private void setUpTank() {
         Spatial tank = assetManager.loadModel("Models/HoverTank/tank.j3o");
         tank.setLocalTranslation(0,0,0);
         tank.setLocalScale(1f);
         rootNode.attachChild(tank);
-        
     }
     
     public static void main(String[] args){
